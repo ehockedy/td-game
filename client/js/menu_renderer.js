a1482697bc742e8c1310218a0e52e7fdbbadc4d9
Edward Hockedy <edhockedy@hotmail.com>
2020-08-26 22:57:04 +0100

    Revamped the main menu
    Changed the start game sprite to pixi js text, and added join game
    button and a title. Currently join gmae also starts the game.
    
    The title is not interactive and just bounces up and down.
    
    This is by no means the final main menu look.

--- client/js/menu_renderer.js
+++ client/js/menu_renderer.js
@@ -6,38 +6,134 @@ const DEFAULT_SPRITE_SIZE_X = 32 // Width of a sprite in the map spritesheet
 const DEFAULT_SPRITE_SIZE_Y = 32 // Height of a sprite in the map spritesheet
 
 let APP_WIDTH = 900
-let APP_HEIGHT = 720
+let APP_HEIGHT = 600
 
 // PIXI application
 let app;
 
-function renderStartGameButton() {
-    const buttonTexture = PIXI.Texture.from('client/img/start_button.png');
-    let buttonSprite = new PIXI.Sprite(buttonTexture);
+// Default style
+let style = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 36,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#00ff99'], // gradient
+    stroke: '#4a1850',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: false,
+    wordWrapWidth: 440,
+})
 
-    buttonSprite.anchor.set(0.5);
-    buttonSprite.x = Math.floor(APP_WIDTH/2)
-    buttonSprite.y = Math.floor(APP_HEIGHT/2)
-    buttonSprite.interactive = true
-    buttonSprite.buttonMode = true; // hand cursor appears when hover over
+let titleStyle = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 72,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#00ff99'], // gradient
+    stroke: '#4a1850',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: false,
+    wordWrapWidth: 440,
+})
 
-    buttonSprite.on("click", onButtonClick)
-                .on("tap", onButtonClick)
 
-    app.stage.addChild(buttonSprite)
+// Text setup
+function renderTitle() {
+    const text = new PIXI.Text('TOWER DEFENCE!', titleStyle);
+    text.anchor.set(0.5);
+    text.x = Math.floor(APP_WIDTH/2)
+    text.y = Math.floor(APP_HEIGHT/3)
+    text.startX = text.x
+    text.startY = text.y
+    text.name = "title"
+    app.stage.addChild(text);
 }
 
-function onButtonClick() {
-    console.log("BUTTON CLICKED")
+function renderStartGameButton() {
+    const text = new PIXI.Text('START GAME', style);
+    text.anchor.set(0.5);
+    text.x = Math.floor(APP_WIDTH/3)
+    text.y = Math.floor(APP_HEIGHT*2/3)
+    text.interactive = true
+    text.buttonMode = true;
+    text.name = "start"
+
+    text.on("click", onStartButtonClick)
+            .on("tap", onStartButtonClick)
+            .on("pointerover", onButtonHover)
+            .on("pointerout", onButtonStopHover)
+
+    app.stage.addChild(text);
+}
+
+function renderJoinGameButton() {
+    const text = new PIXI.Text('JOIN GAME', style);
+    text.anchor.set(0.5);
+    text.x = Math.floor(APP_WIDTH*2/3)
+    text.y = Math.floor(APP_HEIGHT*2/3)
+    text.interactive = true
+    text.buttonMode = true;
+    text.name = "join"
+
+    text.on("click", onJoinButtonClick)
+            .on("tap", onJoinButtonClick)
+            .on("pointerover", onButtonHover)
+            .on("pointerout", onButtonStopHover)
+
+    app.stage.addChild(text);
+}
+
+// Events
+function onButtonHover() {
+    this.scale.set(1.2)
+}
+
+function onButtonStopHover() {
+    this.scale.set(1)
+}
+
+function onStartButtonClick() {
     sendMessage(MSG_TYPES.NEW_GAME)
 }
 
+function onJoinButtonClick() {
+    sendMessage(MSG_TYPES.NEW_GAME)
+}
+
+let dir = 1
+function renderLoop() {
+    // Make the title "bounce"
+    let title = app.stage.getChildByName("title")
+    const range = 10
+    const speed = 1
+
+    let displacement = title.y-title.startY
+    let displacementProportionOfRange = Math.pow((1 - (range - displacement) / range ), 1)
+    let toMove = Math.max(speed * displacementProportionOfRange, 0.01)
+
+    title.y += dir * toMove
+
+    if (title.y > title.startY+range) dir = -1
+    else if (title.y < title.startY) dir = 1
+
+}
+
 function setup() {
-    
+    renderTitle()
     renderStartGameButton()
-    // Start rendering loop
-    // Note that ticker FPS is fixed to monitor rate
-    //app.ticker.add(delta => gameLoop(delta))
+    renderJoinGameButton()
+
+    app.ticker.add(() => renderLoop())
 }
 
 // Sets up PIXI app and run the `setup` function when it's done
@@ -49,7 +145,6 @@ export function startRendering() {
     document.body.appendChild(app.view);
 
     PIXI.Loader.shared
-        .add("client/img/start_button.png")
         .load(setup);
 }
 
