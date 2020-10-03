bdb365e7d6c37bce1cd70e736e01d536af70ce31
Edward Hockedy <edhockedy@hotmail.com>
2020-10-03 14:45:36 +0100

    Added jest test package and first unit tests
    Update the point class, and added some tests for it.
    
    Decided on jest as it eems simple, and very easy to set up.
    
    Will add more tests for existing code.

--- server/js/point.js
+++ server/js/point.js
@@ -2,17 +2,19 @@ const config = require('./constants.js')
 
 
 class Point {
-    constructor(x=null, y=null, row=null, col=null, subrow=null, subcol=null) {
-        let globalForm = (row == null && col == null) // Whether the given coordinates are overall x/y values, or false if row/col & sub row/sub col coordinates
+    constructor(x=0, y=0, sx=0, sy=0) {
+        let globalForm = (arguments.length == 2) // Whether the given coordinates are overall x/y values, or false if row/col & sub row/sub col coordinates
 
-        this.x = globalForm ? x : col * config.SUBGRID_SIZE
-        this.y = globalForm ? y : row * config.SUBGRID_SIZE
+        this.x = globalForm ? x : x * config.SUBGRID_SIZE
+        this.y = globalForm ? y : y * config.SUBGRID_SIZE
 
-        this.col = globalForm ? Math.floor(x / config.SUBGRID_SIZE) : col
-        this.row = globalForm ? Math.floor(y / config.SUBGRID_SIZE) : row
-        this.subcol = globalForm ? Math.floor(x % config.SUBGRID_SIZE) : subcol
-        this.subrow = globalForm ? Math.floor(y % config.SUBGRID_SIZE) : subrow
+        this.col = globalForm ? Math.floor(x / config.SUBGRID_SIZE) : x
+        this.row = globalForm ? Math.floor(y / config.SUBGRID_SIZE) : y
+        this.subcol = globalForm ? Math.floor(x % config.SUBGRID_SIZE) : sx
+        this.subrow = globalForm ? Math.floor(y % config.SUBGRID_SIZE) : sy
     }
+
+
 }
 
 module.exports = {
---
+++ server/test/point.test.js
@@ -0,0 +1,22 @@
+const point = require('../js/point.js')
+const config = require('../js/constants.js')
+
+test("converts local coordinates to global correctly", () => {
+    let testPoint = new point.Point(30, 20, config.SUBGRID_MIDPOINT, config.SUBGRID_MIDPOINT)
+    expect(testPoint.col).toBe(30)
+    expect(testPoint.row).toBe(20)
+    expect(testPoint.subcol).toBe(config.SUBGRID_MIDPOINT)
+    expect(testPoint.subrow).toBe(config.SUBGRID_MIDPOINT)
+    expect(testPoint.x).toBe(30*config.SUBGRID_SIZE)
+    expect(testPoint.y).toBe(20*config.SUBGRID_SIZE)
+})
+
+test("converts global coordinates to local correctly", () => {
+    let testPoint = new point.Point(300, 200)
+    expect(testPoint.col).toBe(Math.floor(300 / config.SUBGRID_SIZE))
+    expect(testPoint.row).toBe(Math.floor(200 / config.SUBGRID_SIZE))
+    expect(testPoint.subcol).toBe(Math.floor(300 % config.SUBGRID_SIZE))
+    expect(testPoint.subrow).toBe(Math.floor(200 % config.SUBGRID_SIZE))
+    expect(testPoint.x).toBe(300)
+    expect(testPoint.y).toBe(200)
+})
