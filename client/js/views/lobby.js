59fe502215522d706ec624224655ff2f7a14237d
Edward Hockedy <edhockedy@hotmail.com>
2020-11-22 22:53:57 +0000

    Added comments

--- client/js/views/lobby.js
+++ client/js/views/lobby.js
@@ -9,7 +9,8 @@ import { getGameID } from "../state.js"
 import { getPositionWithinEquallySpacedObjects } from "../tools.js"
 
 /**
- * This class sets up what will appear in the main menu view.
+ * This class sets up what will appear in the lobby view.
+ * All components positions are defined relative to each other and the boundaries of the menu.
  */
 export class LobbyRenderer {
     constructor() {
@@ -29,24 +30,45 @@ export class LobbyRenderer {
         this.lobbyTitleText.x = popupBoundaryLeft + xMargin;
         this.lobbyTitleText.y = popupBoundaryTop + yMargin;
 
+        // Game ID - this is the code that players can use to join this game
         this.gameIDText = new PIXI.Text("Game code: " + getGameID());
         this.gameIDText.anchor.set(1, 0)
         this.gameIDText.x = popupBoundaryRight - xMargin;
         this.gameIDText.y = popupBoundaryTop + yMargin;
 
-        let mapScale = 420/MAP_WIDTH
+        // Map preview - the primary player (1) can regenrate the map that will be used in the game. The setting will affect the map generated
+        let mapScale = 420/MAP_WIDTH // Scale the map down by this much
         this.map = new MapComponent(this.spriteHandler, mapScale)
         this.map.container.x = popupBoundaryLeft + xMargin
         this.map.container.y = popupBoundaryTop + 80
-        this.regenerateMapButton = new GraphicButton(200, 25, this.map.container.x+this.map.width, this.map.container.y - 5, "Regenerate Map", 20, 0x448877, 1, 1)
+        this.regenerateMapButton = new GraphicButton(
+            200, 25, // width, height
+            this.map.container.x+this.map.width, this.map.container.y - 5, // x, y
+            "Regenerate Map",
+            20, 0x448877, // font size, colour
+            1, 1) // anchor
 
+        // Settings for the game
         let optionWidth = 350
         let optionFontSize = 25
         let optionGap = 25
-        this.mapPathSetting = new GameSetting(popupBoundaryRight - xMargin, this.map.container.y + 20, "Map type", ["Straight", "Winding"], 1, optionFontSize, optionWidth, 1, 0)
-        this.roundsSetting = new GameSetting(popupBoundaryRight - xMargin, this.mapPathSetting.y + this.mapPathSetting.getLocalBounds().height + optionGap, "Rounds", [25, 50, 75, 100], 1, optionFontSize, optionWidth, 1, 0)
-        this.difficultySetting = new GameSetting(popupBoundaryRight - xMargin, this.roundsSetting.y + this.roundsSetting.getLocalBounds().height + optionGap, "Difficulty", ["Easy", "Medium", "Hard", "Very Hard"], 1, optionFontSize, optionWidth, 1, 0)
+        this.mapPathSetting = new GameSetting( // The structure of the map that will be regenerated
+            popupBoundaryRight - xMargin, this.map.container.y + 20, // x, y
+            "Map type", ["Straight", "Winding"], 1, // Setting values
+            optionFontSize, optionWidth, // font size and width (px)
+            1, 0) // Anchor
+        this.roundsSetting = new GameSetting( // The number of rounds to play to beat the game
+            popupBoundaryRight - xMargin, this.mapPathSetting.y + this.mapPathSetting.getLocalBounds().height + optionGap,
+            "Rounds", [25, 50, 75, 100], 1,
+            optionFontSize, optionWidth,
+            1, 0)
+        this.difficultySetting = new GameSetting( // The difficulty of the enemies, and the cost of the towers
+            popupBoundaryRight - xMargin, this.roundsSetting.y + this.roundsSetting.getLocalBounds().height + optionGap,
+            "Difficulty", ["Easy", "Medium", "Hard", "Very Hard"], 1,
+            optionFontSize, optionWidth,
+            1, 0)
 
+        // Lobby members - player that clicked new game is player 1. They can change the game options. When another player joins they fill in the second slot and so on
         let playerSideLen = popupBoundaryBottom - this.map.container.y - this.map.height - yMargin - 20
         let playerMargin = 10
         this.player1 = new Player(1, playerSideLen, playerSideLen, popupBoundaryLeft + xMargin, popupBoundaryBottom-yMargin, 0, 1)
@@ -54,16 +76,18 @@ export class LobbyRenderer {
         this.player3 = new Player(3, playerSideLen, playerSideLen, this.player2.x + playerSideLen + playerMargin, this.player1.y, 0, 1)
         this.player4 = new Player(4, playerSideLen, playerSideLen, this.player3.x + playerSideLen + playerMargin, this.player1.y, 0, 1)
 
+        // Start game button - takes the game setting chsoe and begins the game for all present players
         let startButtonWidth = 180
         let startButtonHeight = 100
         let startButtonStartX = this.player4.x + playerSideLen
         let startButtonStartY = this.player4.y - playerSideLen
         this.startButton = new GraphicButton(
-            startButtonWidth,
-            startButtonHeight,
+            startButtonWidth, startButtonHeight,
             startButtonStartX + getPositionWithinEquallySpacedObjects(1, 1, startButtonWidth, popupBoundaryRight - startButtonStartX),
             startButtonStartY + getPositionWithinEquallySpacedObjects(1, 1, startButtonHeight, playerSideLen),
-            "Start Game", 45, 0xAA88DD, 0.5, 0.5)
+            "Start Game",
+            45, 0xAA88DD, // font size, colour
+            0.5, 0.5) // anchor
     }
 
     loadAssets() {
@@ -80,7 +104,6 @@ export class LobbyRenderer {
         this.spriteHandler.registerContainer(this.startButton)
         this.spriteHandler.registerContainer(this.gameIDText)
         this.spriteHandler.registerContainer(this.lobbyTitleText)
-
         this.map.registerContainer()
         this.spriteHandler.registerContainer(this.regenerateMapButton);
         this.spriteHandler.registerContainer(this.mapPathSetting);
