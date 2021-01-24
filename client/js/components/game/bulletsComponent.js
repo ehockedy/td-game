74514f88cb31d05efe80472bc279d7b769094aba
Edward Hockedy <edhockedy@hotmail.com>
2021-01-24 16:41:49 +0000

    Added custom bullet sprites
    Just the rock bullet for now. It spins as it moves.
    
    Also had to change to chode to actually keep existing bullets, and
    remove them if not present in update.

--- client/js/components/game/bulletsComponent.js
+++ client/js/components/game/bulletsComponent.js
@@ -7,31 +7,51 @@ export class BulletsComponent extends BaseComponent {
         this.spriteSize = spriteSize
         this.spriteSizeMap = spriteSizeMap
         this.bulletStateHashPrev = ""
-        this.bulletSpriteSheet = {}
+        this.rockBullets = []
     }
 
     loadData() {
-        let texture = PIXI.Loader.shared.resources["client/img/bullet_spritesheet.png"].texture
-        this.bulletSpriteSheet["basic"] = []
-        this.bulletSpriteSheet["basic"].push(new PIXI.Texture(texture, new PIXI.Rectangle(0, 0, this.spriteSize, this.spriteSize)))
+        let textures = PIXI.Loader.shared.resources["client/img/bullets/bullets.json"].textures
+        for (let textureName in textures) {
+            if (textureName.includes("rock")) this.rockBullets.push(textures[textureName])
+        }
     }
 
     addBullet(name, type) {
-        let bulletSprite = new PIXI.AnimatedSprite(this.bulletSpriteSheet["basic"])
+        let bulletSprite = new PIXI.Sprite(this.rockBullets[Math.floor(Math.random() * this.rockBullets.length)])
         bulletSprite.name = name
         bulletSprite.anchor.set(0.5)
+        bulletSprite.toRemove = false
         this.addChild(bulletSprite)
     }
 
     update(bulletUpdate) {
-        this.removeChildren() // This only works for bullets as they have no animation - othersiwe would have to keep track of the position in animation loop
-        bulletUpdate["objects"].forEach((bullet) => {
-            this.addBullet(bullet.name, "TODO") // New bullet
-    
-            // Move bullet
-            let newpos = gridPosToMapPos(bullet.position, this.spriteSizeMap, bullet.position.subgridSize)
-            this.getChildByName(bullet.name).x = newpos[0]
-            this.getChildByName(bullet.name).y = newpos[1]
+        //this.children.forEach((bullet) => { bullet.toRemove = true }) // Mark as ready for removal, will be set to false if a bullet with same name is in update
+
+        // Go through the bullets in the list from the server and add them or update positions if exist already
+        bulletUpdate["objects"].forEach((bulletNew) => {
+            let newpos = gridPosToMapPos(bulletNew.position, this.spriteSizeMap, bulletNew.position.subgridSize)
+            let bulletFound = false
+            for (let bulletIdx=0; bulletIdx < this.children.length; bulletIdx += 1) {
+                let bullet = this.children[bulletIdx]
+                if (bulletNew.name == bullet.name) {
+                    bullet.x = newpos[0]
+                    bullet.y = newpos[1]
+                    bullet.angle += 5
+                    bullet.toRemove = false
+                    bulletFound = true
+                    break
+                }
+            }
+            if (!bulletFound) {
+                this.addBullet(bulletNew.name, "TODO") // New bullet
+            }
         })
+
+        for (let bulletIdx = this.children.length - 1; bulletIdx >=0 ; bulletIdx -= 1) {
+            let bullet = this.children[bulletIdx]
+            if (bullet.toRemove) this.removeChild(bullet)
+            else bullet.toRemove = true  // Set to true, on the next update mark as not for removal once bullet seen in update
+        }
     }
 }
--- client/js/main.js
+++ client/js/main.js
@@ -5,7 +5,7 @@ PIXI.Loader.shared
     .add("client/img/tower_spritesheet.png")
     .add("client/img/towers/1-rock_thrower/rock_thrower.json")
     .add("client/img/towers/2-shrapnel_burst/shrapnel_burst.json")
-    .add("client/img/bullet_spritesheet.png")
+    .add("client/img/bullets/bullets.json")
     .add("client/img/infoBox1.png")
     .add("client/img/rocks.json")
     .add("client/img/map_tiles.json")
