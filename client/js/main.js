36bb6c4f10e2aa012ae568acbd1512d1d37c829c
Edward Hockedy <edhockedy@hotmail.com>
2021-05-09 23:48:22 +0100

    Add the new font and load it during the load step
    The new font is called Marble Wasteland. Loading it during start up
    ensures the font is avaiable for use when the first few text components
    load. This is done using the FontFaceObserver package.

--- client/js/main.js
+++ client/js/main.js
@@ -1,25 +1,51 @@
 import { Application } from "./application.js"
 
-PIXI.Loader.shared
-    .add("client/assets/bullets/bullets.json")
-    .add("client/assets/towers/1-rock_thrower/rock_thrower.json")
-    .add("client/assets/towers/2-shrapnel_burst/shrapnel_burst.json")
-    .add("client/assets/towers/3-rock_scatter/rock_scatter.json")
-    .add("client/assets/towers/4-sniper/sniper.json")
-    .add("client/assets/map/base_tiles/base_tiles.json")
-    .add("client/assets/map/land_patterns/land_patterns.json")
-    .add("client/assets/map/land_decorations/land_decorations.json")
-    .add("client/assets/map/path_decorations/path_decorations.json")
-    .add("client/assets/map/path_sides/path_sides.json")
-    .add("client/assets/infoBoxes/infoBoxes.json")
-    .add("client/assets/infoBoxes/towerPopup/towerPopup.json")
-    .load(loadConfig)
+function loadAssets() {
+    return new Promise((resolve) => {
+       PIXI.Loader.shared
+        .add("client/assets/bullets/bullets.json")
+        .add("client/assets/towers/1-rock_thrower/rock_thrower.json")
+        .add("client/assets/towers/2-shrapnel_burst/shrapnel_burst.json")
+        .add("client/assets/towers/3-rock_scatter/rock_scatter.json")
+        .add("client/assets/towers/4-sniper/sniper.json")
+        .add("client/assets/map/base_tiles/base_tiles.json")
+        .add("client/assets/map/land_patterns/land_patterns.json")
+        .add("client/assets/map/land_decorations/land_decorations.json")
+        .add("client/assets/map/path_decorations/path_decorations.json")
+        .add("client/assets/map/path_sides/path_sides.json")
+        .add("client/assets/infoBoxes/infoBoxes.json")
+        .add("client/assets/infoBoxes/towerPopup/towerPopup.json")
+        .load(resolve)
+    })
+}
 
-function loadConfig() {
-    fetch("shared/json/gameConfig.json").then((response) => {
-        response.json().then((data) => {
-            startGame(data)
-        })
+function loadGameConfig() {
+    return new Promise((resolve) => {
+        fetch("shared/json/gameConfig.json").then((response) => {
+            response.json().then((data) => {
+                resolve(data)
+          })
+      })
+    })
+}
+
+function loadFonts() {
+    return new Promise((resolve) => {
+        var font = new FontFaceObserver('MarbleWasteland');
+        font.load().then(
+            resolve
+        )
+    })
+}
+
+function run() {
+    Promise.all([
+        loadAssets(),
+        loadGameConfig(),
+        loadFonts(),
+    ]).then(([asset, gameConfig, font]) => {
+        console.log("Loading complete")
+        startGame(gameConfig)
     })
 }
 
@@ -28,3 +54,5 @@ function startGame(config) {
     let app = new Application(config, socket)
     console.log("New app")
 }
+
+run()
