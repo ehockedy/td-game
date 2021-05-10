46231eef9afe6b49ec0ce28167ed3b3179ae88c2
Edward Hockedy <edhockedy@hotmail.com>
2021-05-10 18:22:02 +0100

    Slight tweak to the counter component
    Made the text a bit smaller so fittesd into the box more nicely.

--- client/js/components/game/ui/counter.js
+++ client/js/components/game/ui/counter.js
@@ -1,17 +1,17 @@
 import { StaticHorizontalMenuOption } from "../../ui_common/horizontalMenuOption.js"
-import { boldTextStyle, COLOURS } from "../../ui_common/style.js"
+import { boldTextStyle, plainTextStyle, COLOURS } from "../../ui_common/style.js"
 
 export class Counter extends StaticHorizontalMenuOption {
     constructor(x, y, width, label, defaultValue, colour=COLOURS.INFO_MID_GREY) {
         super(label, x, y, width, colour, "right")
 
-        this.label = new PIXI.Text(label, boldTextStyle(colour, 36))
+        this.label = new PIXI.Text(label, plainTextStyle(COLOURS.BLACK, 36))
         this.label.anchor.set(0.5)
         this.addText(this.label, 0.6, 0.25)
 
-        this.value = new PIXI.Text("", boldTextStyle(colour, 48))
+        this.value = new PIXI.Text("", boldTextStyle(colour, 42))
         this.value.anchor.set(0.5)
-        this.addText(this.value, 0.6, 0.68)
+        this.addText(this.value, 0.6, 0.62)
         this.update(defaultValue)  // Populate with initial number
     }
 
