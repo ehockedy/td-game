7744adac7e394575da9c47b98a8caff671cc01d9
Edward Hockedy <edhockedy@hotmail.com>
2021-05-13 21:47:34 +0100

    Added animation to counters for when value changes
    This gives confirmation to the player, and also makes the game feel more
    alive and dynamic. Red and moving down for deduction, green and up for
    increase. These directions and colours are natural. The position is
    random in the x direction to avoid clashing of numbers.

--- client/js/components/game/ui/counter.js
+++ client/js/components/game/ui/counter.js
@@ -16,6 +16,39 @@ export class Counter extends StaticHorizontalMenuOption {
     }
 
     update(newValue) {
-        this.value.text = newValue.toString()
+        if (this.value.text != newValue && !isNaN(newValue)) {
+            if (!isNaN(this.value.text)) {
+                this.valueChangeAnimation(parseInt(newValue) - parseInt(this.value.text))
+            }
+            this.value.text = newValue.toString()
+        }
+    }
+
+    valueChangeAnimation(value) {
+        // Triggers an animation showing an increase or decrease in value for the counter
+        let colour = value > 0 ? COLOURS.CONFIRM_GREEN : COLOURS.DENY_RED
+        let sign = value > 0 ? "+" : ""
+        let direction = (value/Math.abs(value))
+
+        let valueChangeText = new PIXI.Text(sign + value.toString() , boldTextStyle(colour, 30))
+        valueChangeText.anchor.set(0.5)
+        valueChangeText.x = this.baseContentOffsetX + this.menuSprite.width/4 + (Math.random() * this.menuSprite.width * 0.5)
+        valueChangeText.y = this.menuSprite.height/2 - 2*direction
+        let moveVelocity = 0.5 * direction * -1  // *-1 since negative direction is up on canvas
+        this.addChild(valueChangeText)
+
+        // Trigger the animation
+        valueChangeText.ticks = 0
+        let animation = setInterval(() => {
+            valueChangeText.ticks += 1
+            valueChangeText.y += moveVelocity
+            if (valueChangeText.ticks > 30) valueChangeText.alpha -= 0.03
+
+            // Disappeared, so remove the sprite and stop the animation loop
+            if (valueChangeText.alpha <= 0) {
+                clearInterval(animation)
+                this.removeChild(valueChangeText)
+            }
+        }, 20)
     }
 }
