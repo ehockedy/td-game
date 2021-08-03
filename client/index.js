00d3a8934c2e2c83803d11482414330da2de4247
Edward Hockedy <edhockedy@hotmail.com>
2021-08-03 21:40:30 +0100

    Added ability to go to lobby after starting game
    This was achieved by transitioning after state change in the new
    Application component

--- client/index.js
+++ client/index.js
@@ -1,11 +1,40 @@
 import { BaseComponent } from "./js/components/game/base/baseComponent.js"
 import { MainMenu } from "./js/views/main_menu_react.js"
+import { Lobby } from "./js/views/lobby_react.js"
 import { io } from "socket.io-client";
+import React, { useEffect, useState } from "react";
 
-const socket = io()
+function Application() {
+    const [view, setView] = useState("main-menu")
+    const [socket, setSocket] = useState()
+
+    useEffect(() => {
+        const socket = io()
+        socket.on("client/view/lobby", () => {
+            setView("lobby")
+        })
+        setSocket(socket)
+        return () => socket.close()
+    }, [])
+
+    return (
+        <div>
+            {
+                view === "main-menu" ? (
+                    <MainMenu socketClient={socket}></MainMenu>
+                ) :
+                view === "lobby" ? (
+                    <Lobby></Lobby>
+                ) :
+                console.log("INVALID VIEW")
+                // TODO add page not found page
+            }
+        </div>
+    )
+}
+
+
 ReactDOM.render(
-    <MainMenu
-        socketClient={socket}
-    ></MainMenu>,
+    <Application/>,
     document.getElementById('root')
   );
---
+++ client/js/views/lobby_react.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class Lobby extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>Lobby</div>
+        )
+    }
+}
