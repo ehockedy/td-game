1c8de68707486e64e2d0cf5f7658c41114e946f0
Edward Hockedy <edhockedy@hotmail.com>
2021-08-12 21:50:06 +0100

    Styled join game text entry form
    This form appears when join game is clicked. WHen cancelled, the start
    and jon game buttons reappear.

--- client/js/views/main_menu_react.js
+++ client/js/views/main_menu_react.js
@@ -27,31 +27,23 @@ function MenuOption(props) {
     )
 }
 
-
-function JoinGameTextBox(props) {
+function TextBoxForm(props) {
     return (
-        <form
-            onSubmit={props.onSubmit}
-            style={{
-                visibility:props.isVisible? "visible" : "hidden",
-                position: "absolute",  // positioned relative to nearest ancestor
-                left: props.leftPos + "%",  // sets position from left edge
-                top: props.topPos + "%",  // sets position from top edge
-                transform: "translate(-50%, -50%)",  // move so that is centered based on centre of element, not top left corner
-            }}>
-            <div>Enter game code below:</div>
-            <label>
-                <input type="text" maxLength="4" value={props.text} onChange={(event) => {props.onChange(event.target.value)}} />
-            </label>
-            <input type="submit" value="Submit" />
-            <button type="button" onClick={props.onClose}>
-                Close
-            </button>
+        <form className="noselect join-game-form" onSubmit={props.onSubmit}>
+            <span className="join-game-title">Enter game code:</span>
+            <input className="join-game-input slanted" type="text" maxLength="4" value={props.text} spellCheck="false" onChange={(event) => {props.onChange(event.target.value)}} />
+            <span className="join-game-button-container slanted">
+                <input className="join-game-button button-inline join-game-button-green" type="submit" value="Submit" />
+                <button className="join-game-button button-inline join-game-button-red" type="button" onClick={props.onClose}>
+                    Cancel
+                </button>
+            </span>
             <br/><br/>
             <div>{props.responseMessage}</div>
         </form>
-    );
+    )
 }
+
 
 export class MainMenu extends React.Component {
     constructor(props) {
@@ -71,73 +63,81 @@ export class MainMenu extends React.Component {
                 <img className="main-menu-logo noselect" src="client/assets/logo/logo.png"></img>
 
                 <div className="main-menu-options noselect">
-                    <MenuOption text="Start Game" leftPos="33" topPos="66"
-                        onClick={() => {
-                            this.props.socket.emit("server/session/start", (gameID, playerID) => {
-                                this.props.setGameIDHandler(gameID)
-                                this.props.setPlayerIDHandler(playerID)
-                            })
-                        }}
-                        disabled={this.state.buttonsDisabled}
-                    ></MenuOption>
-                    <br/><br/>
-                    <MenuOption text="Join Game" leftPos="66" topPos="66"
-                        onClick={() => {
-                            this.setState({
-                                joinGameTextBoxVisible : !this.state.joinGameTextBoxVisible,
-                                buttonsDisabled: true,
-                            })
-                        }}
-                        disabled={this.state.buttonsDisabled}
-                    ></MenuOption>
-                </div>
+                    {
+                        !this.state.joinGameTextBoxVisible ?
 
-                <JoinGameTextBox leftPos="66" topPos="75"
-                    onSubmit={(event) => {
-                        event.preventDefault();  // Prevent page reloading after submit
-                        this.props.socket.emit("server/session/verify", { "gameID": this.state.joinGameText }, (response) => {
-                            if (response["response"] == "fail") { // Game does not exist
-                                // Remove any existing timeout that may override new message output
-                                if (this.state.responseMessageTimeoutFn) {
-                                    clearTimeout(this.state.responseMessageTimeoutFn)
-                                }
+                        <div>
+                            <MenuOption text="Start Game" leftPos="33" topPos="66"
+                                onClick={() => {
+                                    this.props.socket.emit("server/session/start", (gameID, playerID) => {
+                                        this.props.setGameIDHandler(gameID)
+                                        this.props.setPlayerIDHandler(playerID)
+                                    })
+                                }}
+                                disabled={this.state.buttonsDisabled}
+                            ></MenuOption>
 
-                                // Set the new message to disappear after two seconds
-                                let timeoutFn = setTimeout(() => {
-                                    this.setState({responseMessage: ""})
-                                    this.state.responseMessageTimeoutFn = null
-                                }, 2000);
+                            <br/><br/>
 
-                                // Display that the game code did not match an existing game
+                            <MenuOption text="Join Game" leftPos="66" topPos="66"
+                                onClick={() => {
+                                    this.setState({
+                                        joinGameTextBoxVisible : !this.state.joinGameTextBoxVisible,
+                                        buttonsDisabled: true,
+                                    })
+                                }}
+                                disabled={this.state.buttonsDisabled}
+                            ></MenuOption>
+                        </div>
+                        :
+                        <TextBoxForm
+                            onSubmit={(event) => {
+                                event.preventDefault();  // Prevent page reloading after submit
+                                this.props.socket.emit("server/session/verify", { "gameID": this.state.joinGameText }, (response) => {
+                                    if (response["response"] == "fail") { // Game does not exist
+                                        // Remove any existing timeout that may override new message output
+                                        if (this.state.responseMessageTimeoutFn) {
+                                            clearTimeout(this.state.responseMessageTimeoutFn)
+                                        }
+
+                                        // Set the new message to disappear after two seconds
+                                        let timeoutFn = setTimeout(() => {
+                                            this.setState({responseMessage: ""})
+                                            this.state.responseMessageTimeoutFn = null
+                                        }, 2000);
+
+                                        // Display that the game code did not match an existing game
+                                        this.setState({
+                                            responseMessage: "Game not found",
+                                            responseMessageTimeoutFn: timeoutFn,
+                                        })
+                                    } else if (response["response"] == "success") { // Game exists
+                                        this.props.socket.emit("server/session/join", { "gameID": this.state.joinGameText }, (gameID, playerID) => {
+                                            this.props.setGameIDHandler(gameID)
+                                            this.props.setPlayerIDHandler(playerID)
+                                        })
+                                    }
+                                })
+                            }}
+                            onClose={() => {
                                 this.setState({
-                                    responseMessage: "Game not found",
-                                    responseMessageTimeoutFn: timeoutFn,
+                                    joinGameTextBoxVisible : false,
+                                    buttonsDisabled: false,
+                                    joinGameText: "",
                                 })
-                            } else if (response["response"] == "success") { // Game exists
-                                this.props.socket.emit("server/session/join", { "gameID": this.state.joinGameText }, (gameID, playerID) => {
-                                    this.props.setGameIDHandler(gameID)
-                                    this.props.setPlayerIDHandler(playerID)
+                            }}
+                            onChange={(val) => {
+                                this.setState({
+                                    // Enforce upper case, less change of mistaking letters in game code
+                                    joinGameText: val.toUpperCase(),
                                 })
-                            }
-                        })
-                    }}
-                    onClose={() => {
-                        this.setState({
-                            joinGameTextBoxVisible : false,
-                            buttonsDisabled: false,
-                            joinGameText: "",
-                        })
-                    }}
-                    onChange={(val) => {
-                        this.setState({
-                            // Enforce upper case, less change of mistaking letters in game code
-                            joinGameText: val.toUpperCase(),
-                        })
-                    }}
-                    isVisible={this.state.joinGameTextBoxVisible}
-                    text={this.state.joinGameText}
-                    responseMessage={this.state.responseMessage}
-                ></JoinGameTextBox>
+                            }}
+                            isVisible={this.state.joinGameTextBoxVisible}
+                            text={this.state.joinGameText}
+                            responseMessage={this.state.responseMessage}
+                        ></TextBoxForm>
+                    }
+                </div>
             </div>
         )
     }
