bcba77124cc1c2223116b36a0a064bb8fe927f27
Edward Hockedy <edhockedy@hotmail.com>
2021-09-19 18:32:46 +0100

    Put players in point order
    This renders the player information on the game view from top to bottom
    in the order of how many points they have.

--- client/js/components/game/playersToolbar.js
+++ client/js/components/game/playersToolbar.js
@@ -6,12 +6,12 @@ export class PlayersToolbar extends BaseComponent {
     constructor(players) {
         super("playerinfo")
 
-        const gap = 10
+        this.gap = 10
         for(let player in players) {
             const playerData = players[player]
             let ppc = new PlayerPointCounter(0, 0, 200, playerData.displayName, 0, colourHash2Hex(playerData.colour))
             ppc.name = playerData.id
-            ppc.y = this.children.length * (ppc.height + gap)
+            ppc.y = this.children.length * (ppc.height + this.gap)
             this.addChild(ppc)
         }
     }
@@ -40,11 +40,32 @@ export class PlayersToolbar extends BaseComponent {
     // }
 
     update(playersData) {
-        playersData.objects.forEach((player) => {
+        // Order stores the player indexes in the order of how many points they have (highest first)
+        let order = []
+
+        playersData.objects.forEach((player, idx) => {
             let playerInfo = this.getChildByName(player.playerID)
             if (playerInfo != undefined) {
                 playerInfo.update(player.stats.points)
             }
+
+            // Add to back of list
+            order.push(idx)
+            // Search over list, starting form one before one just added (previous smallest)
+            for (let i = (order.length-2); i >= 0; i--) {
+                // If new value bigger, swap
+                if (player.stats.points > playersData.objects[order[i]].stats.points) {
+                    let tmp = order[i]
+                    order[i] = idx
+                    order[i+1] = tmp
+                }
+            }
+        })
+
+        // Adjust the player info so that the highest scoring player appears first
+        order.forEach((playerIdx, posIdx) => {
+            let player = this.getChildByName(playersData.objects[playerIdx].playerID)
+            player.y = posIdx * (player.height + this.gap)
         })
     }
 }
