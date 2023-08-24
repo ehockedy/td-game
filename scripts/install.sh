86e7a51617443295a6c242e0055be31715aebd1b
ehockedy <edhockedy@hotmail.com>
2023-08-24 22:50:43 +0100

    Updated commands and permissions of of install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,18 @@
 # Setup for server to run DOKV on nano EC2 instance
 
 # Install node
-yarn install npm
+yum install npm
 
 # Allocate 4GB of swap memory. Nano instance has only 0.5GB of RAM, not enough to even withstand the
 # npm package installation.
-sudo dd if=/dev/zero of=/swapfile bs=128M count=32
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
+swapoff -a  # First remove any existing swap
+dd if=/dev/zero of=/swapfile bs=128M count=32
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
 
 # Add to file to ensure swap applied if reboots
-sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
 
 # install dependencies
 npm install
